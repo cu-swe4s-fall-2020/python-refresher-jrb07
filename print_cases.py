d0290d3d7cb220bba6c8c6fd5748a38fff7026b3
Jeremy Brown <jermybrown09@gmail.com>
2020-09-12 23:45:09 +0000

    Changed the result_column variable from 4 to the string cases to test the new get_column functionality

--- print_cases.py
+++ print_cases.py
@@ -1,9 +1,9 @@
 
-import my_utils.py as mu
+import my_utils as mu
 
 county='Boulder'
 county_column = 1
-cases_column = 4
+cases_column = "cases"
 file_name = 'covid-19-data/us-counties.csv'
 cases = mu.get_column(file_name, county_column, county, cases_column)
 print(cases)
