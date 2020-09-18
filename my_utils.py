cf9839a27aa5b00be975012f73e7363f06aeca31
Jeremy Brown <jermybrown09@gmail.com>
2020-09-18 02:04:53 +0000

    Cleaned up and corrected grammatically errors in the documentation

--- my_utils.py
+++ my_utils.py
@@ -2,29 +2,6 @@
 """Contains useful functions for file parsing."""
 
 
-def main():
-    desc = 'Opens a file and extracts data from a specific column.'
-
-    parser = argparse.ArgumentParser(description=desc)
-
-    parser.add_argument('--file', dest='file_name',
-                        type=str,
-                        required=True,
-                        help='Name of the file to be opened by the script.')
-
-    parser.add_argument('--column', dest='result_column',
-                        type=int,
-                        required=True,
-                        help='Column of file to be returned by the script.')
-
-    parser.add_argument('--max', dest='max_results',
-                        type=int,
-                        default=10,
-                        help='Number of results to return to stdout.')
-
-    args = parser.parse_args()
-
-
 def get_column(file_name, query_column, query_value, result_column=1):
     """Opens a CSV file and retrieves the desired column data based on inputs
     Inputs:
@@ -34,8 +11,8 @@ def get_column(file_name, query_column, query_value, result_column=1):
                                     The index of the column to query
                 query_value = string
                                     The name of the value to query
-                result_colum = integer or string
-                                    The index or name of the column to return
+                result_column = integer
+                                    The index of the column to return
     """
     results = []
 
@@ -58,7 +35,3 @@ def get_column(file_name, query_column, query_value, result_column=1):
             results.append(A[result_column])
 
     return results
-
-
-if __name__ == '__main__':
-    main()
