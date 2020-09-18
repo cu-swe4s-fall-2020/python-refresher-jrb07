7476eb0e9a09185875a229d6596946c12b0909b2
Jeremy Brown <jermybrown09@gmail.com>
2020-09-18 02:39:41 +0000

    Created shell file to run print_cases.py with 3 different sets of parameters one that will work one that will trigger a FileNotFoundError exception and one that will an IndexError exception.

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
 python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column 4 --county Boulder
+
+python print_cases.py --file covid-2020-data/us-counties.csv --county_column 1 --result_column 4 --county Boulder
+
+python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column 40 --county Boulder
