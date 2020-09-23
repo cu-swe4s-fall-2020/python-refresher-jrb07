939121bcb5e0961853f257eff0105fc1c48e125d
Jeremy Brown <jermybrown09@gmail.com>
2020-09-23 16:29:46 +0000

    Reworked demonstrations to use new argparse variable result_column_str which will handle string input as the result_column

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 
 python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column 4 --county Boulder
 
+python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column_str cases --county Boulder
+
+
+
 python print_cases.py --file covid-2020-data/us-counties.csv --county_column 1 --result_column 4 --county Boulder
 
 python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column 40 --county Boulder
+
+python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column_str casess --county Boulder
