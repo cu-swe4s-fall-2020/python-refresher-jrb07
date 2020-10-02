6c56d41fdd2eb438b9d2fde297cf8896fab6bc8f
Jeremy Brown <jermybrown09@gmail.com>
2020-10-02 01:51:41 +0000

    Added ssshtest to test_print_cases.py Added pycodestyle to run.sh Refactored sys.exit codes for my_utils.py

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ pycodestyle test_my_utils.py
 
 python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column cases --county Boulder  --return_running_average True
 
-python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column cases --county Boulder --return_daily_increment True --return_running_average True
+python print_cases.py 
