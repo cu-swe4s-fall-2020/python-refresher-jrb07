c5e3aa6c680abee4d1457765b0dd03cf0079365c
Jeremy Brown <jermybrown09@gmail.com>
2020-12-04 18:02:30 +0000

    corrected the expected exit codes as I changed all non zero exit codes to 1

--- test_print_cases.sh
+++ test_print_cases.sh
@@ -6,10 +6,10 @@ run test_for_stderr python print_cases.py --file covid-19-data/us-counties.cs --
 assert_exit_code 1
 
 run test_for_stderr python print_cases.py --file covid-19-data/us-counties.csv --county_column 1 --result_column casess --county Boulder --date_column 0
-assert_exit_code 4
+assert_exit_code 1
 
 run test_for_stderr python print_cases.py --file test.csv --county_column 1 --result_column cases --county Boulder --date_column 2
-assert_exit_code 4
+assert_exit_code 1
 
 run test_for_stderr python print_cases.py --file test_date_disorder.csv --county_column 1 --result_column 'date,cases' --county Boulder --date_column 0
-assert_exit_code 4
+assert_exit_code 1
